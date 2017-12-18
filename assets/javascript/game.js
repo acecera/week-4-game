2b780c1a99aaa6145a445549eafe406691dab70e
Anthony Cera <ace_cera@yahoo.com>
2017-12-17 21:42:03 -0800

    added [] to userScore to troubleshoot the problem I had with it not being defined. It didn't help I'm still unable to click on an image and return result.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,40 +5,39 @@ var crystalYellowDiamond = [];
 var crystalEmerald = [];
 var wins = 0;
 var losses = 0;
-var userScore = 0;
-var getRandomNumber = getRandomNumber();
+var userScore = [];
+var getRandomNumber = [];
 },
 
 //This function chooses a random number for the user to guess to 
-var getRandomNumber = getRandomNumber();
 function getRandomNumber() {
     getRandomNumber.innerHTML = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
     $(".crystalScore").text(getRandomNumber);
-},
+}
 
 //function for crystalRuby value 
 function crystalRuby() {
 var crystalRuby = Math.floor((Math.random() * 12) + 1);
 return result;
-},
+}
 
 //function for crystalDiamond value
 function crystalDiamond() {
 crystalDiamond = Math.floor((Math.random() * 12) + 1);
 return result;
-},
+}
 
 //function for crystalYellowDiamond value 
 function crystalYellowDiamond(){
 crystalYellowDiamond = Math.floor((Math.random() * 12) + 1);
 return result;
-},
+}
 
 //function for crystalEmerald value 
 function crystalEmerald(){
 crystalEmerald = Math.floor((Math.random() * 12) + 1);
 return result;
-},
+}
 //This function logs the crystal value to the image for each crystal
 
 $(document).on("click", ".crystalRuby",  function() {
